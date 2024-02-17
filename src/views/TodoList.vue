d43ae7667ab7fd1ce805583323817f8177b44ede
zhaoguowei <2644572487@qq.com>
2024-02-17 02:16:14 +0000

    增加todolist页

---
+++ src/views/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="todolist">
+    <h1>This is an TodoList page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .todolist {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
