f23cedd3edf38e344ba7ad0cb82816bfa5c19a91
zhaoguowei <2644572487@qq.com>
2024-02-17 06:17:02 +0000

    修改钩子函数实现

--- src/hook/todolist.ts
+++ src/hook/todolist.ts
@@ -1,25 +1,28 @@
-import type { IntTodoList } from "@/type/todolist";
-import { reactive } from "vue"
+import type { IntTodoList } from '@/type/todolist'
+import { reactive, watchEffect } from 'vue'
+
+const STORAGE_KEY = 'vue-todomvc'
 
 export default function TodoListHooks() {
+  const todos = reactive<IntTodoList[]>(JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]'))
 
-    type DataType = {
-        list: IntTodoList[];
-        toAddData: IntTodoList;
+  function addTodo(e: KeyboardEvent): void {
+    const input = e.target as HTMLInputElement
+    const value = input.value.trim()
+    if (value) {
+      todos.push({
+        id: Date.now(),
+        title: value,
+        completed: false
+      })
+      input.value = ''
     }
+  }
 
-    const data = reactive<DataType>({
-        list:[],
-        toAddData: {
-            id: 0,
-            title: '',
-            isFinished: false
-        }
-    })
+  // 状态持久化
+  watchEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  })
 
-    function addTodo(){
-        data.list.push({...data.toAddData,id: data.list.length+1})
-    }
-
-    return {data,addTodo}
+  return { todos, addTodo }
 }
