d41080667aa891d1473309cdbf2e156513c6a37c
zhaoguowei <2644572487@qq.com>
2024-02-17 03:51:25 +0000

    增加样式文件

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,15 +1,22 @@
+<script setup lang="ts">
+import TodoListHooks from '@/hook/todolist';
+
+const {data, addTodo} = TodoListHooks()
+
+</script>
+
 <template>
   <div class="todolist">
     <h1>This is an TodoList page</h1>
+    <input class="new-todo" autofocus placeholder="What needs to be done?" @keyup.enter="addTodo">
   </div>
 </template>
 
+<style scoped src="../assets/todolist.css"></style>
 <style>
 @media (min-width: 1024px) {
   .todolist {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
   }
 }
+
 </style>
