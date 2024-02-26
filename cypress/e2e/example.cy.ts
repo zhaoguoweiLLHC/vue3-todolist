abe89b3409942797ac005cde51820a27f8c7976d
zhaoguowei <2644572487@qq.com>
2024-02-26 09:51:26 +0800

    增加hook页面测试用例

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,11 +1,5 @@
 // https://on.cypress.io/api
 
-interface TodoItem {
-  id: number
-  title: string
-  completed: boolean
-}
-
 describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/')
@@ -64,6 +58,41 @@ describe('todolist-hook', () => {
     cy.visit('/todolist-hook')
   })
   it('visits the todolist-hook', () => {
-    cy.contains('h1', 'Todos hook')
+    cy.get('.todoapp>.header>h1').should('have.text', 'Todos hook')
+  })
+  it('add todo', () => {
+    const text = 'add 1'
+    cy.get('.new-todo').type(`${text}{enter}`)
+    cy.get('.todo-list>.todo:last-child').should('have.text', text)
+  })
+
+  it('delete todo', () => {
+    cy.get('.new-todo').type(`add 1{enter}`)
+    cy.get('.todo-list>.todo').should('have.length', 1)
+    cy.get('.todo-list>.todo:last-child').realHover()
+    cy.get('.todo-list>.todo:last-child .destroy').should('be.visible').click()
+    cy.get('.todo-list>.todo').should('have.length', 0)
+  })
+
+  it('edit todo', () => {
+    cy.get('.new-todo').type(`add 1{enter}`)
+    cy.get('.todo-list>.todo:last-child>.view>label').dblclick()
+    cy.get('.todo-list>.todo:last-child>.edit').should('be.visible').type('edit blur')
+    cy.get('.todo-list>.todo:last-child>.edit').blur()
+    cy.get('.todo-list>.todo:last-child>.view>label').should('have.text', 'add 1edit blur')
+  })
+
+  it('edit todo enter', () => {
+    cy.get('.new-todo').type(`add 1{enter}`)
+    cy.get('.todo-list>.todo:last-child>.view>label').dblclick()
+    cy.get('.todo-list>.todo:last-child>.edit').type('edit enter{enter}')
+    cy.get('.todo-list>.todo:last-child>.view>label').should('have.text', 'add 1edit enter')
+  })
+
+  it('edit todo esc', () => {
+    cy.get('.new-todo').type(`add 1{enter}`)
+    cy.get('.todo-list>.todo:last-child>.view>label').dblclick()
+    cy.get('.todo-list>.todo:last-child>.edit').type('edit cancel{esc}')
+    cy.get('.todo-list>.todo:last-child>.view>label').should('have.text', 'add 1')
   })
 })
