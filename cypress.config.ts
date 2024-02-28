34ac5d7be898a35c4c0e71021ea9a52ca810d632
zhaoguowei <2644572487@qq.com>
2024-02-28 11:51:21 +0800

    修改cypress 基础路径

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   projectId: '8k2p6u',
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173'
+    baseUrl: 'http://localhost:4173/vue3-todolist'
   }
 })
