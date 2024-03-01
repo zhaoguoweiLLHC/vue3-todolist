b513a555ba68bf9f619230094528d1e0c1efb512
zhaoguowei <2644572487@qq.com>
2024-03-01 10:43:19 +0800

    测试变量添加了受保护后是否可以通过ci

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      master test dev
-
+      master
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
